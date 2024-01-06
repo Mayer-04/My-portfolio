85b8c104749aba1f2601281587ebc0be97c1a159
Mayer Andres <andres.20.chaves@gmail.com>
2024-01-06 12:15:00 -0500

    Mejoras y cambios en el proyecto, componentes y archivos.

---
+++ src/data/nav-links.ts
@@ -0,0 +1,6 @@
+export const navLinks = [
+  { name: "Inicio", url: "#home" },
+  { name: "Sobre mí", url: "#aboutMe" },
+  { name: "Proyectos", url: "#projects" },
+  { name: "Contacto", url: "#contact" },
+];
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,8 +6,13 @@ export default {
       fontFamily: {
         gabarito: ["Gabarito", "sans-serif"],
       },
-      backgroundColor: {
-        primary: "hsla(241, 41%, 62%, 0.3)",
+      backgroundImage: {
+        "primary-gradient":
+          "linear-gradient(30.6deg, hsla(241, 41%, 62%, 0.3) 28.69%, rgba(124, 130, 177, 0.12) 100%)",
+      },
+      colors: {
+        "mayer-100": "hsla(241, 41%, 62%, 0.3)",
+        "mayer-200": "rgba(124, 130, 177, 0.12)",
       },
     },
   },
