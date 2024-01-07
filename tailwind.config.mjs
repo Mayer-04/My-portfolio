b21bac67e87c1bda0358dfeae1d97b065af972da
Mayer Andres <andres.20.chaves@gmail.com>
2024-01-06 19:11:31 -0500

    Configurando nuevos estilos en algunos componentes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,14 +7,21 @@ export default {
         gabarito: ["Gabarito", "sans-serif"],
       },
       backgroundImage: {
-        "primary-gradient":
+        "gradient-100":
           "linear-gradient(30.6deg, hsla(241, 41%, 62%, 0.3) 28.69%, rgba(124, 130, 177, 0.12) 100%)",
+        "gradient-200":
+          "radial-gradient(77.6% 94.7% at 81.2% 50%, #3D3F8A 0%, #1d1d1d 100%)",
+        "gradient-300":
+          "linear-gradient(64deg, rgb(117, 195, 255) 0%, rgb(110, 231, 183) 100%)",
       },
       colors: {
         "mayer-100": "hsla(241, 41%, 62%, 0.3)",
         "mayer-200": "rgba(124, 130, 177, 0.12)",
+        "mayer-300": "#1D1D1D",
       },
     },
   },
   plugins: [],
 };
+
+// primary-gradient
